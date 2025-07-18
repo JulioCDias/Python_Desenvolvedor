a6e780285e5fe09f880c8285217f8c57a440aeab
Julio Cesar <juliozulu2008@gmail.com>
2025-07-17 23:53:52 -0300

    Input e Typos de dados

---
+++ 1_fundamentos/2_dados.py
@@ -0,0 +1,16 @@
+# PythonFlix
+
+var = "Olá, Mundo!"
+print(var)
+print(type(var))
+
+# Tipos de dados
+# int
+# float
+# string
+# boolean
+
+print(type(1))
+print(type(1.1))
+print(type("1"))
+print(type(True))
---
+++ 1_fundamentos/3_input.py
@@ -0,0 +1,28 @@
+# pythonflix
+
+name = "Top Gun Maverick"
+yaerLaunch = 2023
+noteMovie = 9.5
+planIncluded = False
+
+print(name)
+print(yaerLaunch)
+
+print(type(name))
+print(type(yaerLaunch))
+print(type(noteMovie))
+print(type(planIncluded))
+
+# Input
+
+name = input("Qual o nome do filme? \n")
+yaerLaunch = int(input("Qual o ano de lançamento? \n"))  # casting
+noteMovie = float(input("Qual a nota do filme? \n"))
+
+print(name)
+print(yaerLaunch)
+print(noteMovie)
+
+print(type(name))
+print(type(yaerLaunch))
+print(type(noteMovie))
